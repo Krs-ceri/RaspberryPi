d8234976820db9a5dd36ce7156e6f25997524c91
Merzouk MENHOUR <merzouk@gmail.com>
2022-10-18 21:41:40 +0200

    modifs

--- insert_datas_base/Constantes.hpp
+++ insert_datas_base/Constantes.hpp
@@ -14,16 +14,9 @@ namespace Manage
               {
                      private:
                            string sql_insert = "INSERT INTO temp_datas(identite_capt, temperature, humidity, pressure , gyro_x, gyro_y, gyro_z, accel_x, accel_y, accel_z, seconde, minute, heure, jour, mois, annee) VALUES ('";
-                           //const char * path_data_base = "/usr/local/db/db_temperature.db";
                      public:
                             Constantes(){}
                             ~Constantes(){}
-                            /*
-                            const char * get_url_db()
-                            {
-                                   return this->path_data_base;
-                            }
-                            */
                             string get_query_insert()
                             {
                                    return this->sql_insert;
